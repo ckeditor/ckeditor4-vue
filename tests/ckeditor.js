b43069cfa717263d880c93e46bd76ef7812e6f4e
Kajetan Litwinowicz <k.litwinowicz@cksource.com>
2019-04-30 17:12:54 +0200

    Tests: add unit tests.

--- tests/ckeditor.js
+++ tests/ckeditor.js
@@ -6,23 +6,77 @@
 import Vue from 'vue';
 import { mount } from '@vue/test-utils';
 import CKEditorComponent from '../src/ckeditor';
+import sinon from 'sinon';
+import mockEditor from './_utils/mockeditor';
+
+/* global window CKEDITOR */
 
 describe( 'CKEditor Component', () => {
-	let sandbox, wrapper;
+	const spies = {};
+	let sandbox, wrapper, component, CKEditorNamespace;
 
-	beforeEach( () => {
+	before( () => {
+		CKEditorNamespace = CKEDITOR;
+
+		window.CKEDITOR = mockEditor;
+
+		spies.replace = sinon.spy( CKEDITOR, 'replace' );
+		spies.replace = sinon.spy( CKEDITOR, 'inline' );
+
 		( { wrapper } = createComponent() );
+
+		component = wrapper.vm;
 
 		sandbox = sinon.createSandbox();
 	} );
 
-	afterEach( () => {
-		sandbox.restore();
+	after( () => {
+		window.CKEDITOR = CKEditorNamespace;
 		wrapper.destroy();
+		sandbox.restore();
 	} );
 
 	it( 'component should have a name', () => {
 		expect( CKEditorComponent.name ).to.equal( 'ckeditor' );
+	} );
+
+	it( 'should render', () => {
+		expect( wrapper.html() ).to.equal( '<div><textarea></textarea></div>' );
+	} );
+
+	[
+		{
+			name: 'value',
+			default: ''
+		}, {
+			name: 'type',
+			default: 'classic',
+		}, {
+			name: 'config',
+			default: undefined
+		}, {
+			name: 'tagName',
+			default: 'textarea'
+		}, {
+			name: 'readOnly',
+			default: null
+		}
+	].forEach( prop => {
+		it( `property "${ prop.name }" should have default value`, () => {
+			expect( component[ prop.name ] ).to.equal( prop.default );
+		} );
+	} );
+
+	describe( 'when editor type', () => {
+		it( 'unset should call "CKEDITOR.replace"', () => {
+			sinon.assert.calledOnce( spies.replace );
+		} );
+
+		describe( 'set to inline', () => {
+			before( () => {
+
+			} );
+		} );
 	} );
 
 	function createComponent( props ) {
