a2b57985c8f05ef278ac7f62789c764f6fead4e2
Kajetan Litwinowicz <k.litwinowicz@cksource.com>
2019-05-24 15:27:14 +0200

    Slightly rework getEditorNamespace, so it won't throw when namespace is present, but url is missing. Also it will throw when provided script doesn't contain CKEditor library.

--- src/helpers.js
+++ src/helpers.js
@@ -10,17 +10,21 @@ import loadScript from 'load-script';
 let promise;
 
 export function getEditorNamespace( editorURL ) {
+	if ( 'CKEDITOR' in window ) {
+		return Promise.resolve( CKEDITOR );
+	}
+
 	if ( editorURL.length < 1 ) {
 		throw new TypeError( 'CKEditor URL must be a non-empty string.' );
 	}
 
-	if ( 'CKEDITOR' in window ) {
-		return Promise.resolve( CKEDITOR );
-	} else if ( !promise ) {
+	if ( !promise ) {
 		promise = new Promise( ( scriptResolve, scriptReject ) => {
 			loadScript( editorURL, err => {
 				if ( err ) {
 					scriptReject( err );
+				} else if ( !CKEDITOR ) {
+					scriptReject( 'Script loaded from editorUrl doesn\'t provide CKEDITOR namespace.' );
 				} else {
 					scriptResolve( CKEDITOR );
 					promise = undefined;
