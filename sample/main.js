1016ade7a35530b47cd2b202b81ebebac3605dc4
Kajetan Litwinowicz <k.litwinowicz@cksource.com>
2019-06-05 11:16:17 +0200

    Update and remove reduntant code after changes to component.

--- sample/main.js
+++ sample/main.js
@@ -5,47 +5,20 @@ Vue.use( CKEditor );
 const types = {
 	name: 'types',
 	template: '#types',
-	beforeRouteLeave( to, from, next ) {
-		destroyEditors( this, [
-			'classic',
-			'inline'
-		] ).then( next );
-	},
 	data: () => ( {
 		readOnly: false,
 		hide: false,
 		show: true,
 		editors: {
-			classic: {
-				data: 'Classic editor'
-			},
-			inline: {
-				data: 'Inline editor'
-			}
+			classic: 'Classic editor',
+			inline: 'Inline editor'
 		}
-	} ),
-	methods: {
-		toggleHide( event ) {
-			if ( event.target.checked ) {
-				destroyEditors( this, [
-					'classic',
-					'inline'
-				] ).then( () => {
-					this.hide = true;
-				} );
-			} else {
-				this.hide = false;
-			}
-		}
-	}
+	} )
 };
 
 const events = {
 	name: 'events',
 	template: '#events',
-	beforeRouteLeave( to, from, next ) {
-		destroyEditors( this, [ 'classic' ] ).then( next );
-	},
 	data: () => ( {
 		editorData: 'Check out component events.',
 		events: []
@@ -90,9 +63,6 @@ const events = {
 const binding = {
 	name: 'binding',
 	template: '#binding',
-	beforeRouteLeave( to, from, next ) {
-		destroyEditors( this, [ 'classic' ] ).then( next );
-	},
 	data: () => ( {
 		editorData: 'Check out how two-way data binding works.'
 	} )
@@ -129,7 +99,3 @@ new Vue( {
 	el: '#app',
 	router
 } );
-
-function destroyEditors( component, editorNames ) {
-	return Promise.all( editorNames.map( name => component.$refs[ name ].destroyEditor() ) );
-}
