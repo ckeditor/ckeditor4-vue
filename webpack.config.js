94a2e94d6bc6915a36c065f888cee9fc061a2af5
Jacek Bogdanski <jacekbogd@gmail.com>
2019-12-04 12:57:59 +0100

    Enable legacy build for IE11.

--- webpack.config.js
+++ webpack.config.js
@@ -11,74 +11,82 @@ const path = require( 'path' );
 const webpack = require( 'webpack' );
 const TerserWebpackPlugin = require( 'terser-webpack-plugin' );
 
-module.exports = {
-	mode: 'production',
-	devtool: 'source-map',
+module.exports = env => {
+	const presets = [];
+	let filename = 'ckeditor.js';
 
-	performance: {
-		hints: false
-	},
-
-	entry: path.join( __dirname, 'src', 'index.js' ),
-
-	output: {
-		library: 'CKEditor',
-
-		path: path.join( __dirname, 'dist' ),
-		filename: 'ckeditor.js',
-		libraryTarget: 'umd',
-		libraryExport: 'default'
-	},
-
-	optimization: {
-		minimizer: [
-			new TerserWebpackPlugin( {
-				sourceMap: true,
-				terserOptions: {
-					output: {
-						// Preserve license comments.
-						comments: /^!/
-					}
-				}
-			} )
-		]
-	},
-
-	plugins: [
-		new webpack.BannerPlugin( {
-			banner: `/*!*
- * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md.
- */`,
-			raw: true
-		} ),
-	],
-
-	module: {
-		rules: [
+	if ( env && env.legacy ) {
+		presets.push( [ '@babel/preset-env',
 			{
-				test: /\.js$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/,
-				query: {
-					compact: false,
-					presets: [
-						[ '@babel/preset-env',
-							{
-								useBuiltIns: 'usage',
-								corejs: 3,
-								targets: {
-									browsers: [
-										'last 2 versions',
-										'ie 11'
-									],
-									node: 10
-								}
-							}
-						]
-					]
+				useBuiltIns: 'usage',
+				corejs: 3,
+				targets: {
+					browsers: [
+						'last 2 versions',
+						'ie 11'
+					],
+					node: 10
 				}
 			}
-		]
-	},
+		] );
+
+		filename = 'legacy.js';
+	}
+
+	return {
+		mode: 'production',
+		devtool: 'source-map',
+
+		performance: {
+			hints: false
+		},
+
+		entry: path.join( __dirname, 'src', 'index.js' ),
+
+		output: {
+			filename,
+			library: 'CKEditor',
+			path: path.join( __dirname, 'dist' ),
+			libraryTarget: 'umd',
+			libraryExport: 'default'
+		},
+
+		optimization: {
+			minimizer: [
+				new TerserWebpackPlugin( {
+					sourceMap: true,
+					terserOptions: {
+						output: {
+							// Preserve license comments.
+							comments: /^!/
+						}
+					}
+				} )
+			]
+		},
+
+		plugins: [
+			new webpack.BannerPlugin( {
+				banner: `/*!*
+* @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+* For licensing, see LICENSE.md.
+*/`,
+				raw: true
+			} ),
+		],
+
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					loader: 'babel-loader',
+					exclude: /node_modules/,
+					query: {
+						compact: false,
+						presets
+					}
+				}
+			]
+		}
+	};
 };
