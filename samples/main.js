4234ae87d5fc72d4889231b3edf8e6039c22ba3b
Tomasz Jakut <vepomoc@gmail.com>
2019-12-04 11:58:22 +0100

    Rewrite sample JS to ES5 to support IE11.

--- samples/main.js
+++ samples/main.js
@@ -1,44 +1,47 @@
 /* global Vue VueRouter CKEditor */
+/* eslint-disable */
 
 Vue.use( CKEditor );
 
-const EditorTypesComponent = {
+var EditorTypesComponent = {
 	name: 'editor-types',
 	template: '#editor-types',
-	data: () => ( {
-		readOnly: false,
-		hide: false,
-		show: true,
-		editors: {
-			classic: 'Classic editor',
-			inline: 'Inline editor'
-		}
-	} )
-};
-
-const eventMessages = {
+	data: function(){
+		return {
+			readOnly: false,
+			hide: false,
+			show: true,
+			editors: {
+				classic: 'Classic editor',
+				inline: 'Inline editor'
+			}
+		};
+	}
+},
+eventMessages = {
 	ready: 'Editor is ready',
 	focus: 'Editor is focused',
 	blur: 'Editor is blurred',
 	input: 'Editor has changed'
-};
-
-const EventLoggerComponent = {
+},
+EventLoggerComponent = {
 	name: 'component-events',
 	template: '#component-events',
-	data: () => ( {
-		editorData: 'Check out component events.',
-		events: []
-	} ),
+	data: function() {
+		return {
+			editorData: 'Check out component events.',
+			events: []
+		};
+	},
 	methods: {
-		logEvent( event ) {
-			const previous = this.events[ 0 ];
-			const message = eventMessages[ event ];
+		logEvent: function( event ) {
+			var previous = this.events[ 0 ];
+			var message = eventMessages[ event ];
 
 			if ( previous && event === previous.name ) {
 				previous.counter++;
 
-				const count = previous.counter > 1 ? ` (${ previous.counter })` : '';
+				var count = previous.counter > 1 ? ' (' + previous.counter + ')' : '';
 
 				previous.message = message + count;
 			} else {
@@ -49,26 +52,28 @@ const EventLoggerComponent = {
 				this.events.unshift( {
 					name: event,
 					counter: 1,
-					message
+					message: message
 				} );
 			}
 		}
 	}
 };
 
-const TwoWayBindingComponent = {
+var TwoWayBindingComponent = {
 	name: 'data-binding',
 	template: '#data-binding',
-	data: () => ( {
-		editorData: 'Check out how two-way data binding works.'
-	} )
+	data: function() {
+		return {
+			editorData: 'Check out how two-way data binding works.'
+		};
+	}
 };
 
 Vue.component( 'types', EditorTypesComponent );
 Vue.component( 'events', EventLoggerComponent );
 Vue.component( 'binding', TwoWayBindingComponent );
 
-const routes = [
+var routes = [
 	{
 		path: '/types',
 		component: EditorTypesComponent
@@ -85,10 +90,10 @@ const routes = [
 	}
 ];
 
-const router = new VueRouter( { routes } );
+var router = new VueRouter( { routes: routes } );
 
 /* eslint-disable-next-line no-new */
 new Vue( {
 	el: '#app',
-	router
+	router: router
 } );
