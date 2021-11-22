b8e533a3d037ab6a9293db7cea9ad3adb804b496
Jacek Bogdanski <jacekbogd@gmail.com>
2021-11-22 14:48:27 +0100

    Updated default CDN CKEditor 4 dependency to 4.17.1

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function( config ) {
 		frameworks: [ 'mocha', 'chai', 'sinon' ],
 
 		files: [
-			'https://cdn.ckeditor.com/4.16.2/standard-all/ckeditor.js',
+			'https://cdn.ckeditor.com/4.17.1/standard-all/ckeditor.js',
 			'tests/**/*.js'
 		],
 
