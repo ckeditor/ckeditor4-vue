8cd888bbd61e0c660aab8e921804129726625078
Jacek Bogdanski <jacekbogd@gmail.com>
2023-08-25 10:56:41 +0200

    Added support for license key.

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -28,7 +28,7 @@ export default {
 		},
 		editorUrl: {
 			type: String,
-			default: 'https://cdn.ckeditor.com/4.22.1/standard-all/ckeditor.js'
+			default: 'https://cdn.ckeditor.com/4.23.0-lts/standard-all/ckeditor.js'
 		},
 		config: {
 			type: Object,
